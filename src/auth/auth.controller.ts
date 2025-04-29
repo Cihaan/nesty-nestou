3e4353d3cb8f3b07f833300401dc266c01ef5b47
Cihaan <cihankafadar1@gmail.com>
2025-04-29 12:19:27 +0200

    up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from '../users/user.entity';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' })
   async register(@Body() registerUserDto: RegisterUserDto): Promise<User> {
     return this.authService.register(registerUserDto);
   }
 
   @Post('login')
+  @ApiOperation({ summary: 'Login a user' })
   async login(
     @Body() loginUserDto: LoginUserDto,
   ): Promise<{ accessToken: string }> {
--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsEmail,
@@ -8,17 +9,29 @@ import {
 import { UserRole } from '../../users/user.entity';
 
 export class RegisterUserDto {
+  @ApiProperty({ example: 'test@example.com', description: 'User email' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'password123', description: 'User password' })
   @IsString()
   @MinLength(6)
   password: string;
 
+  @ApiProperty({
+    description: 'User role',
+    required: false,
+    enum: UserRole,
+  })
   @IsOptional()
   @IsString()
   role?: UserRole;
 
+  @ApiProperty({
+    example: ['technology', 'finance'],
+    description: 'User interests',
+    required: false,
+  })
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
